cc757220dcc9238b6e05fd7d45fb22dc7837a2a3
saurabh5234 <saurabhmali1372002@gmail.com>
2025-07-27 00:37:00 +0530

    Fix: corrected import path for sendEmail.mjs

--- automation/newsLetterCron.js
+++ automation/newsLetterCron.js
@@ -1,7 +1,8 @@
 import cron from "node-cron";
 import Job from "../models/jobSchema.js";
 import User from "../models/userSchema.js";
-import sendEmail from "../../utils/sendEmail.mjs";
+import sendEmail from '../utils/sendEmail.mjs';
+
 
 export const newsLetterCron = () => {
     cron.schedule("*/1 * * * *", async () => {
